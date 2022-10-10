d21d7c92d1c0206a52275767b85828fcba8bde03
BharatKumarSaini <bharatskataria@gmail.com>
2022-10-10 13:32:32 +0530

    fix dependecies

--- src/routes/homeStack.tsx
+++ src/routes/homeStack.tsx
@@ -1,6 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import * as React from 'react'
+import React from 'react'
 
 import Home from '../components/home'
 import Scanner from '../components/scanner/index'
